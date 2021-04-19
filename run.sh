9b5b829d321cce5499ca723ee576fcc242363ddd
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2021-04-19 23:13:22 +0300

    remove http server from this git
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>

--- run.sh
+++
@@ -1,1 +0,0 @@
-FLASK_APP=server.py flask run --host=0.0.0.0 --port 80
--- server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import subprocess
-
-app = Flask(__name__)
-
-@app.route('/')
-def main():
-    return subprocess.run('cd /work/src/qemu/vz-8.1; /work/proj/git-range-diff/git-range-diff --meta /work/proj/git-range-diff/porting-to-8.1-new vz-8.1:vz-8.1-base..vz-8.1 v4.1.0 rh-8.1:v4.1.0..vz-8.1-base master-top:v4.1.0..master vz-8.0:our/vz-8.0-base..our/vz-8.0 --jira PSBM-100595 --html', shell=True, stdout=subprocess.PIPE).stdout
-
