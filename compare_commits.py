abede42be930c2cff13eb9cae6cd4ee24f491b88
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2021-09-10 14:57:06 +0300

    allow change commits in-flight
    Allow change commits during interactive comparison process: is user
    change buffer in vim and save it, git rebase would be used to apply the
    changes to current branch.
    
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>

--- compare_commits.py
+++ compare_commits.py
@@ -1,11 +1,12 @@
 import re
 import os
 import subprocess
+from enum import Enum
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, List
 from tempfile import mkstemp
 
-from simple_git import git, git_get_git_dir, git_log1
+from simple_git import git, git_get_git_dir, git_log1, git_log
 
 eat_numbers_subs = tuple((re.compile(a, re.MULTILINE), b) for a, b in
                          (
@@ -149,27 +150,156 @@ def run_vim(f1: str, f2: str, comment_path: Optional[str],
     return CompareResults(ok = code == 200, stop = code not in (0, 200))
 
 
+def text_to_lines(text: str) -> List[str]:
+    if not text:
+        return []
+
+    if text[-1] == '\n':
+        text = text[:-1]
+
+    return text.split('\n')
+
+
+def compile_updated_patch(orig_patch, orig_filtered, updated_filtered):
+    """Given original patch, original patch with some lines substituted by
+    eat_numbers() and updated by user version of the latter thing, restore
+    how original patch should look if apply same changes.
+    """
+    orig_lines = text_to_lines(orig_patch)
+    orig_filtered_lines = text_to_lines(orig_filtered)
+    updated_filtered_lines = text_to_lines(updated_filtered)
+
+    nr = len(orig_lines)
+    assert nr == len(orig_filtered_lines)
+
+    split_indeces = (i for i in range(nr)
+                     if orig_lines[i] != orig_filtered_lines[i])
+
+    spl_ind = next(split_indeces, None)
+    updated_lines = []
+    for line in updated_filtered_lines:
+        if spl_ind is not None and line == orig_filtered_lines[spl_ind]:
+            updated_lines.append(orig_lines[spl_ind])
+            spl_ind = next(split_indeces, None)
+        else:
+            updated_lines.append(line)
+
+    if spl_ind is not None:
+        raise ValueError
+
+    return '\n'.join(updated_lines) + '\n'
+
+
+def check_git_clean_branch() -> str:
+    branch = git('branch --show-current').strip()
+    if not branch:
+        return ''
+
+    status = git('status -uno --porcelain=v1 -b').strip()
+    if status != f'## {branch}':
+        return ''
+
+    return branch
+
+
+class TriWay(Enum):
+    SKIP = 1
+    STOP = 2
+    RETRY = 3
+
+
+def tri_way(problem: str, retry: bool = True, stop_help='') -> TriWay:
+    print(f"""You have modified a patch, but we can not update it: {problem}
+You have {'three' if retry else 'two'} choices:
+1. skip: don't apply the changes, continue interactive process
+2. stop: stop the interactive process now. {stop_help}
+{'3. retry: review same commit again and fix your changes' if retry else ''}
+What to do? [1,2,3]: """)
+    ans = input()
+    while ans not in ('1', '2', '3'):
+        print("you should enter one number, 1 or 2 or 3: ")
+        ans = input()
+
+    return TriWay(int(ans))
+
+
+def apply_patch_changes(commit_hash: str, orig_patch: str, orig_filtered: str,
+                        updated_filtered_fname: str) -> TriWay:
+    with open(updated_filtered_fname) as f:
+        updated_filtered = f.read()
+
+    if updated_filtered == orig_filtered:
+        return TriWay.SKIP
+
+    branch = check_git_clean_branch()
+    if not branch:
+        return tri_way('git is unclean or not at branch', retry=False)
+
+    commit_hash = git_log1('%h', commit_hash)
+    if commit_hash not in git_log('%h', branch):
+        return tri_way('you are trying to modify commit that is not in the '
+                       'current branch')
+
+    try:
+        updated_patch = compile_updated_patch(orig_patch, orig_filtered,
+                                              updated_filtered)
+    except ValueError:
+        return tri_way('unparseable changes in patch')
+
+    try:
+        # checkout prints a lot of information to stderr even on success
+        git(f'checkout {commit_hash}^', stderr=subprocess.DEVNULL)
+    except subprocess.CalledProcessError:
+        return tri_way('git checkout failed', retry=False)
+
+    try:
+        git('am', input=updated_patch)
+    except subprocess.CalledProcessError:
+        w = tri_way('git am failed',
+                    stop_help='You will be left in git-am session. '
+                    f'Use "git am --abort", then "git checkout {branch}" '
+                    'to rollback.')
+        if w != TriWay.STOP:
+            git('am --abort')
+            git('checkout -')
+        return w
+
+    applied_hash = git_log1('%h', 'HEAD')
+    git(f'checkout {branch}', stderr=subprocess.DEVNULL)
+    try:
+        git(f'rebase {applied_hash}')
+    except subprocess.CalledProcessError:
+        w = tri_way('git rebase failed',
+                    stop_help='You will probably be in a git rebase '
+                    'conflict. Use "git rebase --abort" to rollback.')
+        if w != TriWay.STOP:
+            git('rebase --abort')
+        return w
+
+    return TriWay.SKIP
+
+
 def compare_commits(c1, c2, c2_ind=None, comment=None):
     """
     @comment: if None, do simple comparison of two commits and nothing more.
               if str (may be empty), create also temporary file for the
               comment, so that user can modify the comment.
     """
-    c1_text = eat_numbers(git('format-patch --stdout -1 ' + c1),
-                          ignore_empty_lines=False)
-    c2_text = eat_numbers(git('format-patch --stdout -1 ' + c2),
-                          ignore_empty_lines=False)
-    if c1_text == c2_text:
+    c1_orig = git('format-patch --stdout -1 ' + c1)
+    c1_filtered = eat_numbers(c1_orig, ignore_empty_lines=False)
+    c2_orig = git('format-patch --stdout -1 ' + c2)
+    c2_filtered = eat_numbers(c2_orig, ignore_empty_lines=False)
+    if c1_filtered == c2_filtered:
         return CompareResults(equal=True)
 
     f1 = git_log1('/tmp/%h-%f.patch', c1)
     with open(f1, 'w') as f:
-        f.write(c1_text)
+        f.write(c1_filtered)
 
     f2_prefix = '' if c2_ind is None else f'[{c2_ind}]'
     f2 = git_log1(f'/tmp/{f2_prefix}%h-%f.patch', c2)
     with open(f2, 'w') as f:
-        f.write(c2_text)
+        f.write(c2_filtered)
 
     if comment is None:
         comment_path = None
@@ -180,7 +310,24 @@ def compare_commits(c1, c2, c2_ind=None, comment=None):
             f.write(comment)
         meta_tab_opened = comment.strip() != ''
 
-    res = run_vim(f1, f2, comment_path, meta_tab_opened)
+    while True:
+        res = run_vim(f1, f2, comment_path, meta_tab_opened)
+
+        w = apply_patch_changes(c1, c1_orig, c1_filtered, f1)
+        if w == TriWay.RETRY:
+            continue
+        elif w == TriWay.STOP:
+            res.stop = True
+            break
+
+        w = apply_patch_changes(c2, c2_orig, c2_filtered, f2)
+        if w == TriWay.RETRY:
+            continue
+        elif w == TriWay.STOP:
+            res.stop = True
+            break
+
+        break
 
     if comment is not None:
         with open(comment_path) as f:
