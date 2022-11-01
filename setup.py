60bfb059c2c83e6dbd76f0b7ac889d8539c25e94
Vladimir Sementsov-Ogievskiy <vsementsov@yandex-team.ru>
2022-11-02 00:08:42 +0300

    update setup.py
    Add license, readme and the package.
    
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@yandex-team.ru>

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from pathlib import Path
 
 setup(name='git-check-rebase',
       version='0.2',
@@ -6,7 +7,11 @@ setup(name='git-check-rebase',
                   'changes during rebases.',
       author='Vladimir Sementsov-Ogievskiy',
       author_email='vsementsov@yandex-team.ru',
+      long_description=Path('README.rst').read_text(),
+      long_description_content_type='text/x-rst',
+      license_files = ('LICENSE'),
       license='MIT',
       scripts=['git-check-rebase'],
+      packages=['git_check_rebase'],
       url='https://gitlab.com/vsementsov/git-check-rebase',
       install_requires=['tabulate', 'termcolor'])
