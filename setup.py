cabd11efafeef54ec012b5d75b31e7316b677de2
Vladimir Sementsov-Ogievskiy <vsementsov@yandex-team.ru>
2022-11-02 13:15:23 +0300

    Version: 0.3
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@yandex-team.ru>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from pathlib import Path
 
 setup(name='git-check-rebase',
-      version='0.2',
+      version='0.3',
       description='Some useful scripts to operate track history and patch '
                   'changes during rebases.',
       author='Vladimir Sementsov-Ogievskiy',
